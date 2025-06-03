f796c2530c5804581a23b4bb397daad867513ef6
TakuyaMitarai <miterphoto@gmail.com>
2025-06-03 17:55:40 +0900

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Takuya M on 2025/06/03.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sampleTests/sampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  sampleTests.swift
+//  sampleTests
+//
+//  Created by Takuya M on 2025/06/03.
+//
+
+import Testing
+@testable import sample
+
+struct sampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
